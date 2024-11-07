b51877bb597ac88926c83ae71a3cfea15182c624
jpgar <notjp1@gmail.com>
2024-11-07 02:36:51 -0500

    added modulus test in operation testing

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.operations import addition, subtraction, multiplication, division
+from app.operations import addition, subtraction, multiplication, division, modulus
 
 def test_addition():
     """Test Addition"""
@@ -21,3 +21,12 @@ def test_division_zero():
     """Test Division by Zero"""
     with pytest.raises(ValueError, match = "Cannot divide by zero."):
         division(1,0)
+
+def test_modulus_positive():
+    """Test Modulus"""
+    assert modulus(1,1) == 0
+
+def test_modulus_zero():
+    """Test Modulus When Divisor is Zero"""
+    with pytest.raises(ValueError, match = "Divisor cannot be zero."):
+        modulus(1,0)
