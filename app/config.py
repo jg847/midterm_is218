e62abd683c87bdf93b4f55d03991bfa04cfa59b3
jpgar <notjp1@gmail.com>
2024-11-08 21:59:50 -0500

    added history to env config

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 # Get log level from environment
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-LOG_FORMAT = os.getenv(
-    "LOG_FORMAT", "%(asctime)s - %(levelname)s - %(message)s"
-)
+LOG_FORMAT = os.getenv("LOG_FORMAT", "%(asctime)s - %(levelname)s - %(message)s")
+HISTORY_FILE = os.getenv("HISTORY_FILE", "history.csv")
+MAX_HISTORY_ENTRIES= int(os.getenv("MAX_HISTORY_ENTRIES", 10))
